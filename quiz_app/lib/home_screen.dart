d7e8f9bc259595b8ba9fc307be5001dda47256ea
SomeshkumarN <somesh022@hotmail.com>
2025-01-04 13:28:26 -0500

    organized the code for home screen

--- quiz_app/lib/home_screen.dart
+++ quiz_app/lib/home_screen.dart
@@ -4,50 +4,32 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(Object context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              const Color.fromARGB(255, 51, 12, 123),
-              const Color.fromARGB(255, 51, 12, 123)
-            ],
+    return Center(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Image.asset(
+          "assets/images/quiz-logo.png",
+          width: 300,
+        ),
+        const SizedBox(height: 50),
+        const Text(
+          "Learn Flutter the fun way!",
+          style: TextStyle(
+            fontSize: 20,
+            color: const Color.fromARGB(255, 238, 237, 237),
           ),
         ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 100,
+        const SizedBox(height: 30),
+        OutlinedButton(
+          onPressed: () {},
+          child: const Text(
+            'Start Quiz!!',
+            style: TextStyle(
+              fontSize: 20,
+              color: const Color.fromARGB(255, 238, 237, 237),
             ),
-            Image.asset(
-              "assets/images/quiz-logo.png",
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Learn Flutter the fun way!",
-              style: TextStyle(
-                fontSize: 20,
-                color: const Color.fromARGB(255, 238, 237, 237),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'Start Quiz',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: const Color.fromARGB(255, 238, 237, 237),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+          ),
+        )
+      ]),
     );
   }
 }
--- quiz_app/lib/quiz_app.dart
+++ quiz_app/lib/quiz_app.dart
@@ -5,6 +5,20 @@ class QuizApp extends StatelessWidget {
   const QuizApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 51, 12, 123),
+                const Color.fromARGB(255, 51, 12, 123)
+              ],
+            ),
+          ),
+          child: HomeScreen(),
+        ),
+      ),
+    );
   }
 }
