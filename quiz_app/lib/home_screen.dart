6610865b440f637bef377ac4ec710821c57ac7b6
SomeshkumarN <somesh022@hotmail.com>
2025-01-04 13:39:02 -0500

    button has been optimized with style

--- quiz_app/lib/home_screen.dart
+++ quiz_app/lib/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
         Image.asset(
           "assets/images/quiz-logo.png",
           width: 300,
+          color: const Color.fromARGB(150, 255, 255, 255),
         ),
         const SizedBox(height: 50),
         const Text(
@@ -19,16 +20,12 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 30),
-        OutlinedButton(
+        OutlinedButton.icon(
+          icon: const Icon(Icons.arrow_right_alt),
           onPressed: () {},
-          child: const Text(
-            'Start Quiz!!',
-            style: TextStyle(
-              fontSize: 20,
-              color: const Color.fromARGB(255, 238, 237, 237),
-            ),
-          ),
-        )
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+          label: const Text('Start Quiz!!'),
+        ),
       ]),
     );
   }
