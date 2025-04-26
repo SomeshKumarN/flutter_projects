d3886fba1a8781d61cd1bc9ba0e20f60f11cf2ed
SomeshkumarN <somesh022@hotmail.com>
2025-04-26 11:37:33 -0400

    fav_places_app - added the copy of place image to local device

--- fav_places_app/lib/providers/user_places_provider.dart
+++ fav_places_app/lib/providers/user_places_provider.dart
@@ -1,11 +1,22 @@
 import 'package:fav_places_app/model/place.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:path/path.dart' as path;
 
 class UserPlacesProvider extends StateNotifier<List<Place>> {
   UserPlacesProvider() : super([]);
 
-  void addPlace(Place place) {
-    state = [...state, place];
+  void addPlace(Place place) async {
+    final imagePath = await path_provider.getApplicationDocumentsDirectory();
+    final fileName = path.basename(place.image.path);
+    final newImage = await place.image.copy('${imagePath.path}/$fileName');
+
+    final newPlace = Place(
+        title: place.title,
+        image: newImage,
+        location: place.location); // Update the place with the new image path
+    // Save the image to the new path (this is just a placeholder, implement actual saving logic)
+    state = [...state, newPlace];
   }
 
   void removePlace(Place place) {
